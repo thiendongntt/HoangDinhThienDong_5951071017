e58ef0778f6755f37ea9ac6d6610bf6271779912
thiendongntt <thiendonggtvt@gmail.com>
2021-03-22 16:13:49 +0700

    ThienDong

---
+++ script.js
@@ -0,0 +1,13 @@
+axios.get('https://graph.facebook.com/utc2hcmc/posts?access_token=EAAAAZAw4FxQIBAJYJjV2eKVzYZBl2wNnlJ27ww0Kh2dNr1zgw51UnG53YRRO5b6ZAsmloR1PpaXNCIMIRiD0JZA67IUmdZCChYdncF8qppEEKtu8TVPG83GBvz175bTsqadRFbAkzTU1phiZCQjelpfFZBB4B0fijTx3YfhyttWIwZDZD')
+        .then(response => {
+            var output = "";
+            output += " <span class='contain'>Thông tin bài viết số 1</span> </br> Content: " +
+            response.data.data[0].message + "<br>" +
+            "<span class='contain'>Thông tin bài viết số 2</span> </br> Content: " +
+            response.data.data[1].message + "<br>" +
+            "<span class='contain'>Thông tin bài viết số 3</span> </br> Content: " +
+            response.data.data[2].message;
+
+            document.getElementById('main').innerHTML = output;
+        })
+        .catch(error => console.error(error));
